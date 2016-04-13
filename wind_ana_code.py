005d8c3c8f9ce96ef5fa454e60ac60204ea8e3fd
klwalsh26 <klwalsh26@ou.edu>
2016-04-13 15:02:37 -0500

    Plots: - tangential velocity for individual volumes with radius vs tilt - tangential velocity at an elevation angle for time vs radius (like the ones on Dan's poster) *doesn't make subplots*

--- wind_ana_code.py
+++ wind_ana_code.py
@@ -22,89 +22,110 @@ def pts_to_grid(pts_flat,dist,grid):
                 return np.interp((pts_flat-dist),grid,range(0,len(grid)))
 
 day = '20130519'
-tilt_time = day + '231035'
+#tilt_time = day + '231035'
 radar = '/Users/klwalsh/CapstoneGit/NSSLResults/KTLX_%s' %day
-#for dirf in sorted(glob.glob(radar+'/'+day+'*')):
-#    print dirf
+time = []
+vtan2d = []
+tilt = np.arange(0,14)
+for dirf in sorted(glob.glob(radar+'/'+day+'*')):
+    tilt_time = day + dirf[-6::]
+    vtime = dirf[-6::]
+    times = time.append(np.float32(vtime))
+    print dirf
 
-file = open('%s/%s/qd_%s.txt' % (radar,tilt_time,tilt_time))
-data = []
-dd=[]
-for line in file:data.append(line)
-# number of tilts
-numt = len(data)
+    file = open('%s/%s/qd_%s.txt' % (radar,tilt_time,tilt_time))
+    data = []
+    dd=[]
+    for line in file:data.append(line)
+    # number of tilts
+    numt = len(data)
 
-for i in range(numt):dd.append(data[i].split())
-vv_total = np.float32(np.array(dd).reshape((numt,81,81)))
+    for i in range(numt):dd.append(data[i].split())
+    vv_total = np.float32(np.array(dd).reshape((numt,81,81)))
 
-file = open('%s/%s/dd_%s.txt' % (radar,tilt_time,tilt_time))
-data = []
-dd=[]
-for line in file:data.append(line)
-for i in range(numt):dd.append(data[i].split())
-div_total = np.float32(np.array(dd).reshape((numt,81,81)))
+    file = open('%s/%s/dd_%s.txt' % (radar,tilt_time,tilt_time))
+    data = []
+    dd=[]
+    for line in file:data.append(line)
+    for i in range(numt):dd.append(data[i].split())
+    div_total = np.float32(np.array(dd).reshape((numt,81,81)))
 
-file = open('%s/%s/ud_%s.txt' % (radar,tilt_time,tilt_time))
-data = []
-dd=[]
-for line in file:data.append(line)
-for i in range(numt):dd.append(data[i].split())
-u_total = np.float32(np.array(dd).reshape((numt,81,81)))
+    file = open('%s/%s/ud_%s.txt' % (radar,tilt_time,tilt_time))
+    data = []
+    dd=[]
+    for line in file:data.append(line)
+    for i in range(numt):dd.append(data[i].split())
+    u_total = np.float32(np.array(dd).reshape((numt,81,81)))
 
-file = open('%s/%s/vd_%s.txt' % (radar,tilt_time,tilt_time))
-data = []
-dd=[]
-for line in file:data.append(line)
-for i in range(numt):dd.append(data[i].split())
-v_total = np.float32(np.array(dd).reshape((numt,81,81)))
+    file = open('%s/%s/vd_%s.txt' % (radar,tilt_time,tilt_time))
+    data = []
+    dd=[]
+    for line in file:data.append(line)
+    for i in range(numt):dd.append(data[i].split())
+    v_total = np.float32(np.array(dd).reshape((numt,81,81)))
 
-wind_magnitude = np.hypot(u_total,v_total)
+    wind_magnitude = np.hypot(u_total,v_total)
 
-x = np.arange(0,250*81,250)
-y = np.arange(0,250*81,250)
-xgrid,ygrid = np.meshgrid(x,y)
-x_center = 10000.0
-y_center = 10000.0
-radial_spacing = 50 # meters
-azimuthal_spacing = 6 # degress
-radius = np.arange(0,5001,radial_spacing)
-azimuth = np.arange(0,361,azimuthal_spacing)
-Vtan_total = np.zeros((numt,radius.shape[0],azimuth.shape[0])) #3D tangential velocity field (tilt, radius, az), we need to average in azimuth. => make 3D array into 2D array. az avg will then only vary in tilt and radius
-for i in range(numt):
-    xpol, ypol, Vtan, Vrad = polar_disk(x,y,x_center,y_center,radial_spacing,azimuthal_spacing,u_total[i],v_total[i])
-    Vtan_total[i] = Vtan
-# Max tangential velocity with height
-Vtan_max = Vtan_total.max(axis=1).max(axis=1)
+    x = np.arange(0,250*81,250)
+    y = np.arange(0,250*81,250)
+    xgrid,ygrid = np.meshgrid(x,y)
+    x_center = 10000.0
+    y_center = 10000.0
+    radial_spacing = 50 # meters
+    azimuthal_spacing = 6 # degress
+    radius = np.arange(0,5001,radial_spacing)
+    azimuth = np.arange(0,361,azimuthal_spacing)
+    Vtan_total = np.zeros((numt,radius.shape[0],azimuth.shape[0])) #3D tangential velocity field (tilt, radius, az), we need to average in azimuth. => make 3D array into 2D array. az avg will then only vary in tilt and radius
+    for i in range(numt):
+        xpol, ypol, Vtan, Vrad = polar_disk(x,y,x_center,y_center,radial_spacing,azimuthal_spacing,u_total[i],v_total[i])
+        Vtan_total[i] = Vtan
+    # Max tangential velocity with height
+    Vtan_max = Vtan_total.max(axis=1).max(axis=1)
 
-#Tangential Velocity
-Vtan_2D = Vtan_total.mean(axis = 2)
+    #Tangential Velocity
+    Vtan_2D = Vtan_total.mean(axis = 2)
+    vtan2 = vtan2d.append(Vtan_2D)
 
-#Circulation
-az_rad = np.deg2rad(azimuthal_spacing)
-C_2D = np.trapz(Vtan_total, axis = 2, dx = az_rad)*radius
-###PLOTTING###
+    #Circulation
+    az_rad = np.deg2rad(azimuthal_spacing)
+    C_2D = np.trapz(Vtan_total, axis = 2, dx = az_rad)*radius
 
-#Tangential Velocity plots
-plt.figure(1, figsize = (12, 8))
-vtan = plt.contourf(Vtan_2D)
-plt.title("Tangential Velocity")
-plt.xlabel("Radius")
-plt.ylabel("Tilt")
-plt.colorbar(vtan)
-plt.show()
-plt.close()
+    ###PLOTTING###
 
-#Circulation plots
+    #Tangential Velocity plots
+    plt.figure(1, figsize = (12, 8))
+    vtan = plt.contourf(Vtan_2D)
+    plt.title("Tangential Velocity at %s UTC" %vtime)
+    plt.xlabel("Radius (m)")
+    plt.ylabel("Tilt")
+    plt.colorbar(vtan)
+    plt.show()
+#    plt.savefig()
+    plt.close()
+
+time = np.asarray(time)
+vtan2d = np.asarray(vtan2d)
+el = np.array([0.48, 0.88, 1.32, 1.80, 2.42, 3.12, 4.00, 5.10, 6.42, 8.00, 10.02, 12.48, 15.60, 19.51])
+#Tangential Velocity with tilt and time and radius and things.
+for n in range(0, el.shape[0]):
+    height_level = el[n]
+    lev = n
+    vt2d = vtan2d[:,lev,:].T
+    plt.contourf(time, radius, vt2d)
+    plt.title("Tangential Velocity at %s deg elevation" %(el[n]))
+    plt.xlabel("Volume Time UTC")
+    plt.ylabel("Radius (m)")
+    plt.show()
+    #plt.savefig()
+    plt.close()
+
+
+'''#Circulation plots
 plt.figure(2, figsize = (12, 8))
 circ = plt.contourf(C_2D)
 plt.title("Circulation")
 plt.xlabel("Radius")
 plt.ylabel("Tilt")
 plt.colorbar(circ)
-plt.show()
-plt.close()
-
-# plot example
-'''plt.contourf(xpol,ypol,Vtan_total[5])
 plt.show()
 plt.close()'''
